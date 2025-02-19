ecf3f1cc6e1a51cb6d8a542c3a55093cd32b2772
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2025-02-19 14:51:36 +0700

    fetch all products

--- components/Products.tsx
+++ components/Products.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { getAllProducts, Product } from "@/app/services/productService";
+import Link from "next/link";
 
 const Products = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -29,26 +30,28 @@ const Products = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {products.length > 0 ? (
             products.map((product) => (
-              <div
-                key={product._id}
-                className="bg-white p-6 rounded-lg shadow-lg"
-              >
-                <img
-                  src={product.image_url}
-                  alt={product.name}
-                  className="w-full h-48 object-cover rounded-t-lg"
-                />
-                <h3 className="text-xl font-bold text-purple-800 mt-4">
-                  {product.name}
-                </h3>
-                <p className="text-gray-600 mt-2">{product.description}</p>
-                <p className="text-gray-700 font-semibold mt-2">
-                  ${product.price.toFixed(2)}
-                </p>
-                <button className="mt-4 bg-purple-600 text-white px-6 py-2 rounded-full hover:bg-purple-700 transition duration-300">
-                  Add to Cart
-                </button>
-              </div>
+              <Link href={`/products/${product._id}`} key={product._id}>
+                <div
+                  key={product._id}
+                  className="bg-white p-6 rounded-lg shadow-lg"
+                >
+                  <img
+                    src={product.image_url}
+                    alt={product.name}
+                    className="w-full h-48 object-cover rounded-t-lg"
+                  />
+                  <h3 className="text-xl font-bold text-purple-800 mt-4">
+                    {product.name}
+                  </h3>
+                  <p className="text-gray-600 mt-2">{product.description}</p>
+                  <p className="text-gray-700 font-semibold mt-2">
+                    ${product.price.toFixed(2)}
+                  </p>
+                  <button className="mt-4 bg-purple-600 text-white px-6 py-2 rounded-full hover:bg-purple-700 transition duration-300">
+                    Add to Cart
+                  </button>
+                </div>
+              </Link>
             ))
           ) : (
             <p className="text-center text-gray-500">No products found.</p>
