6bd51c4c8b3196bff4b5444c98e60c1817f3d2f6
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2025-02-25 17:14:33 +0700

    check if token is expired or not

--- app/(page)/profile/page.tsx
+++ app/(page)/profile/page.tsx
@@ -7,12 +7,21 @@ import Error from "@/components/Error";
 import Loading from "@/components/Loading";
 
 import { getUserById } from "@/app/services/userService";
+import { useRouter } from "next/navigation";
 
 const ProfilePage: FC = () => {
   const [user, setUser] = useState<any>(null);
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const access_token = localStorage.getItem("access_token");
 
   useEffect(() => {
+    if (!access_token) {
+      Error("Not Logged In", "Please log in to view your profile.");
+      router.push("/sign-in");
+      return;
+    }
+
     const getUserProfile = async () => {
       try {
         const userData = await getUserById();
