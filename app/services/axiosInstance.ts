a1943543a73595c0df0f82a0a3e9aa1f65c93437
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2025-03-02 15:49:55 +0700

    adjust

---
+++ app/services/axiosInstance.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { API_URL } from "@/config";
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("access_token");
+
+      if (typeof window !== "undefined") {
+        window.location.href = "/sign-in";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
